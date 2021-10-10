bacdf3c351728c980f1c223866bb04e2d4f6dd12
Elena Gorshenina <elenkagorshenina19.07.14@gmail.com>
2021-10-10 14:50:24 +0300

    Deleted class ErrorInfo, edited ReplenishCard and MoneyTransferTest

--- src/test/java/ru/netology/page/ReplenishCard.java
+++ src/test/java/ru/netology/page/ReplenishCard.java
@@ -2,25 +2,35 @@ package ru.netology.page;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import lombok.val;
 import ru.netology.data.DataHelper;
 
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.value;
 import static com.codeborne.selenide.Selenide.$;
+import static io.restassured.RestAssured.given;
 
 public class ReplenishCard {
     private SelenideElement headingH1 = $("h1");
     private SelenideElement amount = $("[data-test-id='amount'] input");
     private SelenideElement from = $("[data-test-id='from'] input");
     private SelenideElement replenishButton = $("[data-test-id='action-transfer']");
+    private static SelenideElement errorWindow = $(".notification__content");
 
     public ReplenishCard() {
         headingH1.shouldBe(Condition.visible);
     }
 
-    public DashboardPage replenish (String sum, DataHelper.NumberCard numberCard){
-            amount.setValue(sum);
-            from.setValue(numberCard.getNumberCard());
-            replenishButton.click();
-            return new DashboardPage();
-        }
+    public DashboardPage replenish(String sum, DataHelper.NumberCard numberCard) {
+        amount.setValue(sum);
+        from.setValue(numberCard.getNumberCard());
+        replenishButton.click();
+        return new DashboardPage();
+    }
+
+    public static ReplenishCard errorCard() {
+        errorWindow.shouldHave(exactText("Недействительный номер карты"));
+        return new ReplenishCard();
+    }
 
 }
--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -2,9 +2,7 @@ package ru.netology.test;
 
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelper;
-import ru.netology.page.ErrorInfo;
 import ru.netology.page.LoginPageV1;
-import ru.netology.page.ReplenishCard;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,8 +11,7 @@ public class MoneyTransferTest {
 
     @Test
     void shouldTransferMoneyBetweenOwnCardsV1() {
-        open("http://localhost:9999");
-        var loginPage = new LoginPageV1();
+        var loginPage = open("http://localhost:9999", LoginPageV1.class);
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
         var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
@@ -25,16 +22,15 @@ public class MoneyTransferTest {
         String sum = "100";
         var numberCard = DataHelper.getNumberCard2();
         var replenish = replenishCard1.replenish(sum, numberCard);
-        int balance1after= dashBoardPage.getFirstCardBalance();
-        int balance2after= dashBoardPage.getSecondCardBalance();
+        int balance1after = dashBoardPage.getFirstCardBalance();
+        int balance2after = dashBoardPage.getSecondCardBalance();
         assertEquals(balance1before + Integer.parseInt(sum), balance1after);
         assertEquals(balance2before - Integer.parseInt(sum), balance2after);
     }
 
     @Test
     void shouldTransferMoneyBetweenOwnCardsV2() {
-        open("http://localhost:9999");
-        var loginPage = new LoginPageV1();
+        var loginPage = open("http://localhost:9999", LoginPageV1.class);
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
         var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
@@ -45,16 +41,15 @@ public class MoneyTransferTest {
         String sum = "300";
         var numberCard = DataHelper.getNumberCard1();
         var replenish = replenishCard2.replenish(sum, numberCard);
-        int balance1after= dashBoardPage.getFirstCardBalance();
-        int balance2after= dashBoardPage.getSecondCardBalance();
+        int balance1after = dashBoardPage.getFirstCardBalance();
+        int balance2after = dashBoardPage.getSecondCardBalance();
         assertEquals(balance1before - Integer.parseInt(sum), balance1after);
         assertEquals(balance2before + Integer.parseInt(sum), balance2after);
     }
 
     @Test
     void noShouldTransferMoneyIfSumMoreBalance() {
-        open("http://localhost:9999");
-        var loginPage = new LoginPageV1();
+        var loginPage = open("http://localhost:9999", LoginPageV1.class);
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
         var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
@@ -65,16 +60,15 @@ public class MoneyTransferTest {
         String sum = "15000";
         var numberCard = DataHelper.getNumberCard2();
         var replenish = replenishCard1.replenish(sum, numberCard);
-        int balance1after= dashBoardPage.getFirstCardBalance();
-        int balance2after= dashBoardPage.getSecondCardBalance();
+        int balance1after = dashBoardPage.getFirstCardBalance();
+        int balance2after = dashBoardPage.getSecondCardBalance();
         assertEquals(balance1before, balance1after);
         assertEquals(balance2before, balance2after);
     }
 
     @Test
     void errorIfNumberCardNoValid() {
-        open("http://localhost:9999");
-        var loginPage = new LoginPageV1();
+        var loginPage = open("http://localhost:9999", LoginPageV1.class);
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
         var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
@@ -85,7 +79,11 @@ public class MoneyTransferTest {
         String sum = "200";
         var numberCard = DataHelper.getNumberCardNoValid();
         var replenish = replenishCard1.replenish(sum, numberCard);
-        var error = ErrorInfo.errorCard();
+        var replenish1 = replenishCard1.errorCard();
+        int balance1after = dashBoardPage.getFirstCardBalance();
+        int balance2after = dashBoardPage.getSecondCardBalance();
+        assertEquals(balance1before, balance1after);
+        assertEquals(balance2before, balance2after);
     }
 
 }
